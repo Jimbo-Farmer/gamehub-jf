4a4e13398ecdd94775a45ffbef1e7a4de3f61fce
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2021-01-08 11:56:52 +0100

    JS for store

--- root/js/logic.js
+++ root/js/logic.js
@@ -1,11 +1,30 @@
+// hamburger menu
+
 var navbar = document.querySelector("nav")
 var ham = document.querySelector(".hamburger")
 
 ham.addEventListener("click", toggleHamburger)
 function toggleHamburger(){
-    navbar.classList.toggle("showNav")
-    ham.classList.toggle("showClose")
-    console.log("clicked")
-  }
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+  console.log("clicked")
+}
+
+
+// clickable store items
+
+var items = document.querySelectorAll(".featured-item")
+var itemImage = document.querySelector(".featured-item img")
+var itemInfo = document.querySelector(".featured-item .item-information")
+
+
+for (var i = 0; i < items.length; i++) {
+items[i].addEventListener("click", toggleItemInfo)
+function toggleItemInfo(){
+  // item.classList.toggle("show-item-info")
+  this.classList.toggle("item-info-show")
+  this.classList.toggle("image-blur")
+}
+}
 
 
