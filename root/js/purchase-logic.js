e7fe2ae6eba6c77285c21149e8e212dc6b390054
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2021-01-13 16:04:31 +0100

    store complete

--- root/js/purchase-logic.js
+++ root/js/purchase-logic.js
@@ -1,18 +1,29 @@
 // Purchase process
-var add = document.querySelector("#add");
+var addToBasket = document.querySelector("#add");
 var text = document.querySelector(".prompt");
 var priceDisplay = document.querySelector(".price");
 var proceed = document.querySelector(".proceed");
 var basket = document.querySelector("#basket")
 
 
-add.addEventListener("click", togglePrice)
+addToBasket.addEventListener("click", togglePrice)
 function togglePrice(){
   text.textContent = "Added!";
   priceDisplay.id =("hide");
-  add.src = ("icons and logos/add-to-cart-pressed.png");
+  addToBasket.src = ("icons and logos/add-to-cart-pressed.png");
   basket.src = ("icons and logos/full-basket.png");
   proceed.classList.toggle("show-proceed");
 }
 
 
+// hamburger menu
+
+var navbar = document.querySelector("nav")
+var ham = document.querySelector(".hamburger")
+
+ham.addEventListener("click", toggleHamburger)
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+  console.log("clicked")
+}
