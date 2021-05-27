f0645d9a91ca720d6301562f5fba5e557dfb4f04
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2021-05-27 13:11:28 +0200

    Update store.js

--- root/javascript/store.js
+++ root/javascript/store.js
@@ -100,17 +100,17 @@ for (let i = 0; i < items.length; i++) {
     }
   })
 }
-for (let i = 0; i < items.length; i++) {
-  items[i].addEventListener("mouseover", toggleItemInfo)
-  function toggleItemInfo(){
-    this.classList.add("extra-info-show");
-  }
-}
-for (let i = 0; i < items.length; i++) {
-  items[i].addEventListener("mouseout", toggleItemInfo)
-  function toggleItemInfo(){
-    this.classList.remove("extra-info-show");
-    addToCart.innerHTML = `Add to cart`;
-  }
-}
+// for (let i = 0; i < items.length; i++) {
+//   items[i].addEventListener("mouseover", toggleItemInfo)
+//   function toggleItemInfo(){
+//     this.classList.add("extra-info-show");
+//   }
+// }
+// for (let i = 0; i < items.length; i++) {
+//   items[i].addEventListener("mouseout", toggleItemInfo)
+//   function toggleItemInfo(){
+//     this.classList.remove("extra-info-show");
+//     addToCart.innerHTML = `Add to cart`;
+//   }
+// }
 
