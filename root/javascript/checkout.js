599faccc4d0d3faa3bf6fd541bcf8068ad05f25c
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2021-10-08 14:47:26 +0200

    Various, see 1st Year Projects site

--- root/javascript/checkout.js
+++ root/javascript/checkout.js
@@ -1,37 +1,50 @@
 const shoppingCart = JSON.parse(localStorage.getItem("cartList"));
+
+
+const basketQty = JSON.parse(localStorage.getItem("numberOfItems"));
+      if((!isNaN(basketQty) && basketQty > 0)){
+        var cartQuantity = basketQty;
+      } else cartQuantity = 0;
+
 console.log(shoppingCart);
+
 const summary = document.querySelector(".summary");
 const totals = document.querySelectorAll(".item-total");
 const paymentButton = document.querySelector("#checkout-submit");
 
-
 function total(array){
     let sum = 0;
     for(let i = 0; i < array.length; i++){
-        sum += (array[i].prices.price)*(array[i].inBasket);
-        
+        sum += (array[i].prices.price)*(array[i].inBasket);  
     }
     return sum;
 }
 
 let shipping = 5;
 
-if(!shoppingCart){
+if((!shoppingCart)||(shoppingCart.length === 0)){
     summary.innerHTML = `<tbody>Your Cart Is Empty :(</tbody>`
 } else {
+    drawSummary();  
+}
+
+
+function drawSummary(){
     summary.innerHTML = `
     <tbody>
         <tr class="column-headers">
             <td tabindex="0">Item</td>
             <td tabindex="0">Qty</td>
+            <td></td>
             <td tabindex="0">Unit Price</td>
-            <td tabindex="0">Price</td>
+            <td tabindex="0">Price</td>   
         </tr>`; 
         
         for(let i = 0; i < shoppingCart.length; i++){
             summary.innerHTML += `<tr class="item-info">
             <td tabindex="0">${shoppingCart[i].name}</td>
             <td tabindex="0">${shoppingCart[i].inBasket}</td>
+            <td class="remove">x</td>
             <td tabindex="0">$${shoppingCart[i].prices.price}</td>
             <td tabindex="0" class="item-total">$${parseInt(shoppingCart[i].prices.price) * parseInt(shoppingCart[i].inBasket)}</td>
             </tr>`
@@ -39,16 +52,19 @@ if(!shoppingCart){
     summary.innerHTML +=`<tr>
             <td></td>
             <td></td>
+            <td></td>
             <td tabindex="0" class="subtotal">Subtotal</td>
-            <td tabindex="0" class="subtotal">$${total(shoppingCart)}</td>
+            <td tabindex="0" class="subtotal">$${total(shoppingCart)}</td>  
         </tr>
         <tr>
             <td></td>
             <td></td>
+            <td></td>
             <td tabindex="0">Shipping</td>
-            <td tabindex="0">$${shipping}</td>
+            <td tabindex="0">$${shipping}</td>   
         </tr>
         <tr class="total">
+            <td></td>
             <td></td>
             <td></td>
             <td tabindex="0" class="total">Total</td>
@@ -56,6 +72,25 @@ if(!shoppingCart){
         </tr>
     </tbody>          
     `;
+    const removeButtons = document.querySelectorAll(".remove");
+    for(let i = 0; i < removeButtons.length; i++){
+        removeButtons[i].addEventListener("click", function(){
+            if(shoppingCart[i].inBasket > 0){
+                shoppingCart[i].inBasket -=1;
+                cartQuantity -= 1;
+                if(shoppingCart[i].inBasket === 0){
+                    shoppingCart.splice(i,1);
+                }
+                localStorage.setItem("numberOfItems", JSON.stringify(cartQuantity));
+                localStorage.setItem("cartList", JSON.stringify(shoppingCart));
+                if(shoppingCart.length === 0){
+                    summary.innerHTML = `<tbody>Your Cart Is Empty :(</tbody>`
+                } else {
+                    drawSummary();  
+                }
+            }
+        })
+    }
 }
 
 
--- root/javascript/sell.js
+++ root/javascript/sell.js
@@ -1,15 +1,94 @@
-//coin animate on sale
-const coin = document.querySelector(".send");
+let alreadyLoggedIn = JSON.parse(localStorage.getItem("loggedIn"));
+
+const page = document.querySelector(".page");
+
+if(!alreadyLoggedIn){
+    page.innerHTML = `
+    <div class="page-title">
+        <h1>Sell your used games</h1>
+    </div>
+    <div class="sell-info">You must be logged in to post a game for sale
+    <a class="link-to-login" href="account.html">Go to Login</a>
+    </div>`
+}
+
+const coin = document.querySelector("#send");
 const submitMessage = document.querySelector(".submit-message");
 const options = document.querySelector(".success");
 const terms = document.querySelector("#terms");
 
+const gameDetails = document.querySelector("#game-details");
+const unopened = document.querySelector("#unopened");
+const good = document.querySelector("#good");
+const fair = document.querySelector("#fair");
+const poor = document.querySelector("#poor");
+const sellingPrice = document.querySelector("#selling-price");
 
-coin.addEventListener("click", toggleAnimate)
+const gameTitleError = document.querySelector("#game-title-error");
+const gameConditionError = document.querySelector("#game-condition-error");
+const sellingPriceError = document.querySelector("#selling-price-error");
+const termsError = document.querySelector("#terms-error");
+
+let sent = false;
+
+coin.addEventListener("click", function(){
+    flagErrors();
+    console.log(validate());
+    if(validate()){
+        toggleAnimate();
+    }
+})
+
+
 function toggleAnimate(){
-    if(terms.checked){
-        this.classList.toggle("animate");
+    if(!sent){
+        coin.classList.toggle("animate");
         options.classList.remove("hidden");
         submitMessage.textContent = "Sent!";
+        sent= true;
+        
     }
 }
+
+function flagErrors(){
+    event.preventDefault();
+    if(lengthCheck(gameDetails.value.trim().length, 1)){
+        gameTitleError.style.visibility = "hidden";
+    } else {
+        gameTitleError.style.visibility = "unset";
+    }
+    if(unopened.checked || good.checked || fair.checked || poor.checked){
+        gameConditionError.style.visibility = "hidden";
+    } else {
+        gameConditionError.style.visibility = "unset";
+    }
+    if(!isNaN(parseInt(sellingPrice.value))){
+        sellingPriceError.style.visibility = "hidden";
+    } else {
+        sellingPriceError.style.visibility = "unset";
+    }
+    if(terms.checked){
+        termsError.style.visibility = "hidden";
+    } else {
+        termsError.style.visibility = "unset";
+    }
+}
+
+
+function validate(){
+    if((lengthCheck(gameDetails.value.trim().length, 1)) && (unopened.checked || good.checked || fair.checked || poor.checked) && (!isNaN(parseInt(sellingPrice.value))) && terms.checked){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function lengthCheck(input, desiredLength){
+    if(input >= desiredLength){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
